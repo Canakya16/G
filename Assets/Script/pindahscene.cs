8881634c85e4e6626a15bbaf3c9a420898c4b685
Canakya Bima Ananda Putra <canakyabima18@gmail.com>
2023-10-11 22:06:01 +0700

    add project to github

---
+++ Assets/Script/pindahscene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pindahscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pindah(){
+        SceneManager.LoadScene("main 2");
+    }
+
+    public void PindahSemua(string namaScene){
+        SceneManager.LoadScene(namaScene);
+    }
+
+    //script animasi
+    public void scale(float scale){
+        transform.localScale = new Vector2 (1/scale,1*scale);
+    }
+}
